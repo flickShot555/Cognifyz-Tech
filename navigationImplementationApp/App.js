fbad1c1d90d540a9b2542b3ecad0e7e7e1883e0d
Abbas Inayatullah Khan <abbas1795khan@gmail.com>
2025-02-15 23:59:42 +0500

    till new

--- navigationImplementationApp/App.js
+++ navigationImplementationApp/App.js
@@ -7,6 +7,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <Text> Changes you make will automatically reload.</Text>
       <Text>Shake your phone to open the developer menu.</Text>
+      <Text> Press Ctrl+M to open the menu on desktop.</Text>
       <StatusBar style="auto" />
     </View>
   );
